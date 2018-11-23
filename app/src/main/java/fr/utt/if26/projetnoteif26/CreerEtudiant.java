51969d7322b1ad9c90a366227b73a65f73da8faa
lefrancp <paul.lefranc@utt.fr>
2018-11-23 13:38:26 +0100

    Finition de l'interface de CreerEtudiant + association id <=> variable de CreerEtudiant.java

--- app/src/main/java/fr/utt/if26/projetnoteif26/CreerEtudiant.java
+++ app/src/main/java/fr/utt/if26/projetnoteif26/CreerEtudiant.java
@@ -2,12 +2,33 @@ package fr.utt.if26.projetnoteif26;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class CreerEtudiant extends AppCompatActivity {
+
+    EditText numeroEtu;
+    EditText nom;
+    EditText prenom;
+    Spinner spinnerAdmission;
+    Spinner spinnerFiliere;
+    Button submit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_creer_etudiant);
+
+        numeroEtu = findViewById(R.id.activity_creer_etudiant_edit_text_num_id);
+        nom = findViewById(R.id.activity_creer_etudiant_edit_text_nom_id);
+        prenom = findViewById(R.id.activity_creer_etudiant_edit_text_prenom_id);
+        spinnerAdmission = findViewById(R.id.activity_creer_etudiant_spinner_admission_id);
+        spinnerFiliere = findViewById(R.id.activity_creer_etudiant_spinner_filiere_id);
+        submit = findViewById(R.id.activity_creer_etudiant_button_id);
+
+        
+
+
     }
 }
